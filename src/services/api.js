8db84566c30fa7bfba3b5d89171a73914e4e8ac3
vimal raj <vimalraj87464@gmail.com>
2025-08-25 17:22:29 +0530

    commit

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   // baseURL: "http://localhost:5000/api", // backend base url
-  baseURL: "https://dbuzzz-backend-z39p.onrender.com", // backend base url
+  baseURL: "https://dbuzzz-backend-z39p.onrender.com/api", // backend base url
   withCredentials: true, // cookie support
 });
 
